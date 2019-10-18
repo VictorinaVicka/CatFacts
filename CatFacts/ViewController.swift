aca002dda4aeb5066f3d608f7665af75148e1670
VictorinaVicka <city_br@mail.ru>
2019-10-18 14:15:33 +0300

    Initial Commit

---
+++ CatFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatFacts
+//
+//  Created by Виктория Воробьева on 18/10/2019.
+//  Copyright © 2019 Виктория Воробьева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
